e167c33685ba8dcecf0a3f277bc8db17079f40a9
野中明 <nonaka04@hotmail.co.jp>
2016-02-22 12:40:02 +0900

    Initial Commit

---
+++ おみくじ/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  おみくじ
+//
+//  Created by 野中明 on 2016/02/22.
+//  Copyright © 2016年 practice. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
